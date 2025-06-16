5420cc7ab9d65f27dbc6bddf0a5e3842d765ff97
thebrownproject <fraserbrown@live.com>
2025-06-16 21:03:49 +1000

    Revert "Dev update sidebar contents+top nav bar"

--- components/layout/sidebar/index.ts
+++
@@ -1,6 +0,0 @@
-export { SidebarApp } from "./sidebar-app";
-export { NavPlatform } from "./sidebar-nav-platform";
-export { NavProjects } from "./sidebar-nav-projects";
-export { NavQuickActions } from "./sidebar-nav-quick-actions";
-export { NavUser } from "./sidebar-nav-user";
-export { TeamSwitcher } from "./sidebar-team-switcher";
--- components/layout/topbar/index.ts
+++
@@ -1,4 +0,0 @@
-export { TopBarApp } from "./topbar-app";
-export { TopBarBreadcrumbs } from "./topbar-breadcrumbs";
-export { TopBarSearch } from "./topbar-search";
-export { TopBarActions } from "./topbar-actions";
